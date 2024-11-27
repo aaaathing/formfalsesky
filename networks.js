ae592bb6e3e727c869d536444a5c038cfe8d6bf2
aaaathing <aarontao950@gmail.com>
2024-11-26 16:11:07 -0800

    add to networks

--- networks.js
+++ networks.js
@@ -11,12 +11,30 @@ const XX1vmActiveThreshold = 0.01
 // (2-abs(x))*x
 function contrast(x){return (2-abs(x))*x}
 
+const XCALDThr = 0.0001, XCALDRev = 0.1
+function XCAL(x, th){
+	return (x < XCALDThr) ? 0 : (x > th * XCALDRev) ? (x - th) : (-x * ((1-XCALDRev)/XCALDRev))
+}
+const SIGOff = 0.5
+function SIG(w){
+	return 1 / (1 + (SIGOff * (1-w)/w)**2.5)
+}
+
 
 const {max,min,abs} = Math
 class Syn{
-	Wt
-	LWt //linear
+	Wt = 0
+	LWt = 0 //linear
 	otherSide
+	doLern(Send){
+		let srs = Send.AvgSLrn * this.otherSide.AvgSLrn
+		let srm = Send.AvgM * this.otherSide.AvgM
+		let dwt = XCAL(srs, srm) + this.otherSide.AvgLLrn * XCAL(srs, this.otherSide.AvgL)
+		//todo: maybe normalize dwt
+		//todo: maybe balance
+		this.LWt += dwt
+		this.Wt = SIG(this.LWt)
+	}
 }
 class Ne{
 	Act = 0
@@ -26,12 +44,14 @@ class Ne{
 	Ext //external input
 	Inet = 0
 	Vm = 0
-	AvgSS
-	AvgS
-	AvgM
-	AvgL
-	AvgLLrn
-	AvgSLrn
+	AvgSS = 0
+	AvgS = 0
+	AvgM = 0
+	AvgL = 0
+	AvgLLrn = 0
+	AvgSLrn = 0
+	ActM //minus phase
+	ActP //plus phase
 	syns = []
 	x;y;z
 	constructor(x, y, z){
@@ -72,6 +92,24 @@ class Ne{
 		}
 		this.Act += (1/3.3) * (newAct-this.Act)
 	}
+	updateLernAvgs(){
+		this.AvgSS += (1/2)*(this.Act-this.AvgSS)
+		this.AvgS += (1/2)*(this.Act-this.AvgS)
+		this.AvgM += (1/10)*(this.Act-this.AvgM)
+		this.AvgSLrn = (1-0.1) * this.AvgS + 0.1 * this.AvgM
+	}
+	updateLernAvgsAtMinusPhaseEnd(){
+		this.ActM = this.Act
+	}
+	updateLernAvgsAtPlusPhaseEnd(){
+		this.ActP = this.Act
+		this.AvgL += (1/10) * (2.5 * this.AvgM - this.AvgL); this.AvgL = max(this.AvgL, 0.2) //only once per trial
+		this.AvgLLrn = ((0.0001 - 0.5) / (2.5 - 0.0001)) * (this.AvgL - 0.0001)
+		this.AvgLLrn *= max(abs(this.ActP-this.ActM), 0.01)
+	}
+	doLern(){
+		for(let s of this.syns) s.doLern(this)
+	}
 }
 class Layer{
 	nodes = []
@@ -87,17 +125,25 @@ class Layer{
 			}
 		}
 	}
+	tick(){
+
+	}
 	getNode(x,y=0,z=0){
 		return this.nodes[(x*this.w+y)*this.h+z]
 	}
 }
 
 
-let n=new Ne(0,0,0)
+let n=new Ne(0,0,0), s=new Syn()
+s.Wt=s.LWt=1
+s.otherSide=new Ne(1,0,0)
+n.syns.push(s)
 function testAct(ge,gi){
 	n.Ge=ge;n.Gi=gi
 	n.updateActive()
-	console.log( n.Act)
+	console.log(n)
+	console.log(s.LWt)
+	console.log(s.otherSide)
 }
 let it=0
 setInterval(()=>{
